336e1a9f174389931626819abfa35869128c305d
jakmro <jakubmroz314@gmail.com>
2024-05-26 02:25:11 +0200

    NST

---
+++ README.md
@@ -0,0 +1,64 @@
+# Neural Style Transfer
+This project demonstrates the integration of PyTorch and OpenCV to process images and videos. It uses a pre-trained models to transfer style to specified to an image or a video.
+
+## Prerequisites
+- C++ Compiler (e.g., GCC or Clang)
+- CMake
+- OpenCV 4.x
+- LibTorch (PyTorch C++ API)
+
+Ensure that you have installed the required libraries (OpenCV and LibTorch) on your system.
+
+## Build project:
+    ```bash
+    mkdir build && cd build
+    cmake ..
+    make
+    ```
+
+## Usage
+```bash
+./nst type <resolution_width> <resolution_height> <src> <style> <dst>
+```
+
+Command Parameters:
+- `type:` Specifies the type of input, either image or video. This parameter tells the application how to handle the input file, either as a single image or a sequence of images in a video.
+
+- `resolution_width`: The width of the output file in pixels. This defines the horizontal size of the processed output.
+
+- `resolution_height`: The height of the output file in pixels. This defines the vertical size of the processed output.
+
+- `src`: The path to the source file. This is the input image or video file that you want to apply the style transfer to.
+
+- `style`: The name of the style model to use for the transfer. This parameter should correspond to a pre-trained model file that implements a particular artistic style.
+
+- `dst`: The destination path where the stylized output will be saved. This should be a valid path where the application can write the processed image or video.
+
+## Example
+Image style transfer:
+```bash
+./nst image 640 480 ./input/example.jpg example_model ./output/processed_example.jpg
+```
+
+Video style transfer:
+```bash
+./nst video 640 480 ./input/example.mp4 example_model ./output/processed_example.mp4
+```
+
+Warning: The resolution width and height will be rounded to the smallest number divisible by 4 greater than the given number.
+
+## Styles
+
+The `./models` directory contains pre-trained models for four different styles. You can apply any of these styles to your images or videos by specifying the appropriate model name in the usage command. Below are the descriptions and model names for each style:
+
+1. **candy** - Produces vibrant and colorful candy-like textures.
+2. **mosaic** - Applies a classic mosaic texture, reminiscent of ancient tiled artwork.
+3. **rain_princess** - Transfers the soft and dreamy aesthetics of the "Rain Princess" painting.
+4. **udnie** - Infuses images with the swirling modern art style seen in Francis Picabia's "Udnie".
+
+## Acknowledgements
+This application utilizes style transfer models that were originally developed by contributors to the [Fast Neural Style](https://github.com/pytorch/examples/tree/main/fast_neural_style) project, hosted on PyTorch's official examples repository.
+
+I have adapted these models by tracing them with PyTorch to create TorchScript versions suitable for deployment in c++ environment.
+
+
---
+++ main.cpp
@@ -0,0 +1,118 @@
+#include <torch/torch.h>
+#include <torch/script.h>
+#include <opencv2/opencv.hpp>
+#include <string>
+
+
+cv::Mat load_image(std::string image_path) {
+    std::string path = cv::samples::findFile(image_path);
+    cv::Mat image = cv::imread(path);
+    return image;
+}
+
+torch::Tensor image_to_tensor(cv::Mat& image) {
+    cv::cvtColor(image, image, cv::COLOR_BGR2RGB);
+    image.convertTo(image, CV_32FC3);
+    torch::Tensor tensor = torch::from_blob(
+        image.data,
+        { image.rows, image.cols, image.channels() },
+        torch::kFloat32);
+    tensor = tensor.permute({ 2, 0, 1 });
+    tensor = tensor.unsqueeze(0);
+    return tensor;
+}
+
+torch::jit::Module load_model(std::string model_path) {
+    torch::jit::Module model = torch::jit::load(model_path);
+    model.eval();
+    return model;
+}
+
+cv::Mat tensor_to_image(torch::Tensor tensor) {
+    tensor = tensor.squeeze().detach();
+    tensor = tensor.permute({ 1, 2, 0 });
+    tensor = tensor.contiguous();
+    tensor = tensor.clamp(0, 255).to(torch::kU8);
+    tensor = tensor.to(torch::kCPU);
+    int64_t height = tensor.size(0);
+    int64_t width = tensor.size(1);
+
+    cv::Mat mat(height, width, CV_8UC3, tensor.data_ptr());
+    cv::cvtColor(mat, mat, cv::COLOR_RGB2BGR);
+    return mat;
+}
+
+
+int main(int argc, char* argv[]) {
+    std::string input_type(argv[1]);
+    int resolution_width = atoi(argv[2]);
+    int resolution_height = atoi(argv[3]);
+    std::string input_path(argv[4]);
+    std::string model_name(argv[5]);
+    std::string output_path(argv[6]);
+
+    // make width and height divisible by 4
+    resolution_width = (resolution_width + 3) / 4 * 4;
+    resolution_height = (resolution_height + 3) / 4 * 4;
+
+    // loading model from file
+    torch::jit::Module model = load_model("../models/traced_" + model_name + ".pt");
+
+    if (input_type == "image") {
+        // loading image from file
+        cv::Mat input = load_image(input_path);
+        cv::resize(input, input, cv::Size(resolution_width, resolution_height));
+
+        // transforming image to input data for model
+        torch::Tensor input_tensor = image_to_tensor(input);
+        std::vector<torch::jit::IValue> inputs;
+        inputs.push_back(input_tensor);
+
+        // running model
+        torch::Tensor output_tensor = model.forward(inputs).toTensor();
+
+        // transforming models output to image format
+        cv::Mat output = tensor_to_image(output_tensor);
+
+        // saving image to file
+        cv::imwrite(output_path, output);
+    }
+    else if (input_type == "video") {
+        cv::VideoCapture cap(input_path);
+        int fps = cap.get(cv::CAP_PROP_FPS);
+        cv::VideoWriter video(
+            output_path,
+            cv::VideoWriter::fourcc('m', 'p', '4', 'v'),
+            fps,
+            cv::Size(resolution_width, resolution_height)
+        );
+
+        while (true) {
+            cv::Mat frame;
+            cap >> frame;
+
+            if (frame.empty()) {
+                break;
+            }
+
+            // resizing frame
+            cv::resize(frame, frame, cv::Size(resolution_width, resolution_height));
+
+            // transforming image to input data for model
+            torch::Tensor input_tensor = image_to_tensor(frame);
+            std::vector<torch::jit::IValue> inputs;
+            inputs.push_back(input_tensor);
+
+            // running model
+            torch::Tensor output_tensor = model.forward(inputs).toTensor();
+
+            // transforming models output to image format
+            cv::Mat output = tensor_to_image(output_tensor);
+
+            video.write(output);
+        }
+
+        cap.release();
+        video.release();
+    }
+}
